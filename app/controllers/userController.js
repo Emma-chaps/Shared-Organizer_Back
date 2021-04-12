bab97b48c37301a4ab5aaa6ac7d34d2dfc286f67
Jeremy Kalvar <jeremykalvar@gmail.com>
2021-04-12 22:29:30 +0200

    deletion of last console.logs

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcrypt');
 exports.createAdmin = async (req, res, next) => {
   try {
     let { groupName, firstname, email, password, icon } = req.body;
-    console.log('req.body:', req.body);
     const role = 3;
 
     // cleans body elements
