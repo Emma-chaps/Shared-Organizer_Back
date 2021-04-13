cdbb1e4adaec04fe9b97fd5a10c5b2862de7107e
Jeremy Kalvar <jeremykalvar@gmail.com>
2021-04-13 22:06:39 +0200

    createWidget: error handling, title and labelId verification

--- app/controllers/widgetController.js
+++ app/controllers/widgetController.js
@@ -1,10 +1,34 @@
-const { Group, Member } = require('../models');
+const { Widget, Label } = require('../models');
 
-exports.getWidgetsByDate = async (req, res, next) => {
+exports.createWidget = async (req, res, next) => {
   try {
-    const { period } = req.params;
-    console.log('period:', period);
+    const { title, listStyle, description, color, date, labelId } = req.body;
+
+    //database required fields verification
+    if (!title || !labelId) throw new Error('title and labelId are required');
+
+    //get associated label
+    const dbLabel = await Label.findByPk(labelId);
+
+    //widget creation
+    const widget = await Widget.create({
+      title,
+      listStyle,
+      description,
+      color,
+      date,
+      id_label: dbLabel.id,
+    });
+
+    //sends back json if all is ok
+    res.json({
+      success: true,
+      widget,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
   }
 };
