97e0d227bab01cd59a69b5a67aad5e6dbc1dd658
Jeremy Kalvar <jeremykalvar@gmail.com>
2021-04-19 19:00:03 +0200

    /family-settings/group - /dashboard/week/:weekNb - /dashboard/day/:dayNb - /dashboard/month/:monthNb all ok

--- app/models/index.js
+++ app/models/index.js
@@ -1,8 +1,6 @@
 const Group = require('./Group');
 const Member = require('./Member');
 const Widget = require('./Widget');
-const Field = require('./Field');
-const Label = require('./Label');
 
 Group.hasMany(Member, {
   foreignKey: 'id_group',
@@ -12,26 +10,6 @@ Group.hasMany(Member, {
 Member.belongsTo(Group, {
   foreignKey: 'id_group',
   as: 'group',
-});
-
-Widget.hasMany(Field, {
-  foreignKey: 'id_widget',
-  as: 'fields',
-});
-
-Field.belongsTo(Widget, {
-  foreignKey: 'id_widget',
-  as: 'widget',
-});
-
-Label.hasMany(Widget, {
-  foreignKey: 'id_label',
-  as: 'widgets',
-});
-
-Widget.belongsTo(Label, {
-  foreignKey: 'id_label',
-  as: 'label',
 });
 
 Group.hasMany(Widget, {
@@ -44,18 +22,18 @@ Widget.belongsTo(Group, {
   as: 'group',
 });
 
-// Widget.belongsToMany(Member, {
-//   as: 'members',
-//   through: 'member_widget',
-//   foreignKey: 'id_widget',
-//   otherKey: 'id_member',
-// });
+Widget.belongsToMany(Member, {
+  as: 'members',
+  through: 'member_widget',
+  foreignKey: 'id_widget',
+  otherKey: 'id_member',
+});
 
-// Member.belongsToMany(Widget, {
-//   as: 'widgets',
-//   through: 'member_widget',
-//   foreignKey: 'id_member',
-//   otherKey: 'id_widget',
-// });
+Member.belongsToMany(Widget, {
+  as: 'widgets',
+  through: 'member_widget',
+  foreignKey: 'id_member',
+  otherKey: 'id_widget',
+});
 
-module.exports = { Group, Member, Widget, Field, Label };
+module.exports = { Group, Member, Widget };
