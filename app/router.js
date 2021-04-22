bacd9dea1c4524f0d0b3896c59a9eeee630b70e5
Jeremy Kalvar <jeremykalvar@gmail.com>
2021-04-22 15:26:59 +0200

    triple dynamic road to get widgets tested and functionnal

--- app/router.js
+++ app/router.js
@@ -1,47 +1,50 @@
-const express = require("express");
-const dashboardController = require("./controllers/dashboardController");
-const settingsController = require("./controllers/settingsController");
-const userController = require("./controllers/userController");
-const widgetController = require("./controllers/widgetController");
-const securityController = require("./controllers/securityController");
+const express = require('express');
+const dashboardController = require('./controllers/dashboardController');
+const settingsController = require('./controllers/settingsController');
+const userController = require('./controllers/userController');
+const widgetController = require('./controllers/widgetController');
+const securityController = require('./controllers/securityController');
 
 const router = express.Router();
 
 // landing page
-router.post("/signup", userController.createAdmin);
-router.post("/login", userController.login);
+router.post('/signup', userController.createAdmin);
+router.post('/login', userController.login);
 
 // dashboard get
+// router.get(
+//   '/dashboard/month/:monthNb',
+//   securityController.authorizationMiddleware,
+//   dashboardController.getAllWidgetFromMonth,
+// );
+// router.get(
+//   '/dashboard/week/:weekNb',
+//   securityController.authorizationMiddleware,
+//   dashboardController.getAllWidgetFromWeek,
+// );
+// router.get(
+//   '/dashboard/day/:dayNb',
+//   securityController.authorizationMiddleware,
+//   dashboardController.getAllWidgetFromDay,
+// );
+// all widgets
 router.get(
-
-  '/dashboard/month/:monthNb',
+  '/dashboard/:year/:range/:dateNb',
   securityController.authorizationMiddleware,
-  dashboardController.getAllWidgetFromMonth,
-);
-router.get(
-  '/dashboard/week/:weekNb',
-  securityController.authorizationMiddleware,
-  dashboardController.getAllWidgetFromWeek,
-);
-router.get(
-  '/dashboard/day/:dayNb',
-  securityController.authorizationMiddleware,
-  dashboardController.getAllWidgetFromDay,
+  dashboardController.getWidgets,
 );
 
 // widget creation/update/delete
 router.post(
-
   '/dashboard/widgets/create',
   securityController.authorizationMiddleware,
   widgetController.createWidget,
 );
-router.patch("/:group/dashboard/widgets");
-router.delete("/:group/dashboard/widgets");
+router.patch('/:group/dashboard/widgets');
+router.delete('/:group/dashboard/widgets');
 
 // edit settings admin only
 router.get(
-
   '/family-settings',
   securityController.authorizationMiddleware,
   securityController.adminChecker,
@@ -55,27 +58,26 @@ router.post(
 );
 
 router.patch(
-  "/family-settings/group",
+  '/family-settings/group',
   securityController.authorizationMiddleware,
   securityController.adminChecker,
   settingsController.changeGroupName,
-
 );
 
-router.patch("/:group/family-settings");
-router.delete("/:group/family-settings");
+router.patch('/:group/family-settings');
+router.delete('/:group/family-settings');
 
 //contact page
-router.post("/contact");
-router.get("/contact");
+router.post('/contact');
+router.get('/contact');
 
 // about us
-router.get("/about-us");
+router.get('/about-us');
 
 router.use((request, response) => {
   response.status(404).json({
     success: false,
-    error: "⚠ Service does not exist !",
+    error: '⚠ Service does not exist !',
   });
 });
 
