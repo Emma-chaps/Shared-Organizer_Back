ee6f92a91fd3d75b9732f45064aec9c934e44ff1
Emma <em.charpin@gmail.com>
2021-05-26 10:49:55 +0200

    aws deploy

--- app/router.js
+++ app/router.js
@@ -8,8 +8,8 @@ const securityController = require('./controllers/securityController');
 const router = express.Router();
 
 // landing page
-router.post('/signup', userController.createAdmin, userController.login);
-router.post('/login', userController.login);
+router.post('/api/signup', userController.createAdmin, userController.login);
+router.post('/api/login', userController.login);
 
 router.get(
   '/renew-token',
@@ -27,7 +27,7 @@ router.get(
 
 // IMPROVED
 router.get(
-  '/all-widgets/:year/:month',
+  '/api/all-widgets/:year/:month',
   securityController.authorizationMiddleware,
   dashboardController.getAllWidgets
 );
@@ -42,72 +42,72 @@ router.get(
 
 // widget creation/update/delete
 router.post(
-  '/dashboard/widgets/create',
+  '/api/dashboard/widgets/create',
   securityController.authorizationMiddleware,
   widgetController.createWidget
 );
 router.patch(
-  '/update-widget/:id',
+  '/api/update-widget/:id',
   securityController.authorizationMiddleware,
   widgetController.updateWidget
 );
 router.delete(
-  '/delete-widget/:id',
+  '/api/delete-widget/:id',
   securityController.authorizationMiddleware,
   widgetController.deleteWidget
 );
 
 // edit settings admin only
 router.get(
-  '/group-infos',
+  '/api/group-infos',
   securityController.authorizationMiddleware,
   // securityController.adminChecker,
   settingsController.getgroupInfo
 );
 router.post(
-  '/group-settings',
+  '/api/group-settings',
   securityController.authorizationMiddleware,
   securityController.adminChecker,
   settingsController.addMember
 );
 
 router.patch(
-  '/group-settings/group',
+  '/api/group-settings/group',
   securityController.authorizationMiddleware,
   securityController.adminChecker,
   settingsController.changeGroupName
 );
 
 router.patch(
-  '/group-settings/members',
+  '/api/group-settings/members',
   securityController.authorizationMiddleware,
   securityController.adminChecker,
   settingsController.editGroupData
 );
 
 router.patch(
-  '/group-settings/member/password',
+  '/api/group-settings/member/password',
   securityController.authorizationMiddleware,
   securityController.adminChecker,
   settingsController.editPassword
 );
 
 router.delete(
-  '/group-settings/member/delete/:id',
+  '/api/group-settings/member/delete/:id',
   securityController.authorizationMiddleware,
   securityController.adminChecker,
   settingsController.deleteMember
 );
 
-router.patch('/:group/group-settings');
-router.delete('/:group/group-settings');
+router.patch('/api/:group/group-settings');
+router.delete('/api/:group/group-settings');
 
 //contact page
-router.post('/contact');
-router.get('/contact');
+router.post('/api/contact');
+router.get('/api/contact');
 
 // about us
-router.get('/about-us');
+router.get('/api/about-us');
 
 router.use((request, response) => {
   response.status(404).json({
